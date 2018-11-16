3988534d00fc8420e892799c51c03f25881a817d
haibo.wang <haibo.wang@fengjr.com>
2018-11-16 16:59:18 +0800

    m

--- src/redux/reducers/visitor.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var actions = require('../actions')
-module.exports = function (state = null, action = {}) {
-  if (action.type === actions.SET_VISITOR) {
-    return action.visitor
-  }
-  return state
-}
